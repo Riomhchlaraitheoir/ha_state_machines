76bfdf2a7086568d355805cae53840429d95a288
dylan <dylanocuinn4@gmail.com>
2024-10-14 18:04:03 +0100

    Add OptionsFlow

--- custom_components/state_machines/config_flow.py
+++ custom_components/state_machines/config_flow.py
@@ -2,16 +2,18 @@ from typing import Any
 
 from homeassistant import config_entries, data_entry_flow
 import voluptuous as vol
+from homeassistant.config_entries import ConfigEntry, OptionsFlow
 from homeassistant.const import CONF_NAME
 from .const import DOMAIN
 
-class StateMachineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 0
     MINOR_VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> data_entry_flow.FlowResult:
+        errors = {}
         if user_input is not None:
             await self.async_set_unique_id(user_input[CONF_NAME])
             self._abort_if_unique_id_configured()
@@ -19,5 +21,30 @@ class StateMachineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required(CONF_NAME): str})
+            data_schema=vol.Schema({vol.Required(CONF_NAME): str}),
+            errors=errors
+        )
+
+    def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
+        return OptionsFlow(config_entry)
+
+
+class OptionsFlowHandler(OptionsFlow):
+    def __init__(self, config_entry: ConfigEntry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict[str, Any]):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "show_things",
+                        default=self.config_entry.options.get("show_things")
+                    ): bool
+                }
+            )
         )
